6d7bd011b6b024fc0a3548be6ccddbafc451c212
EdwardGit <pocketknifegithub@163.com>
2020-08-20 16:52:49 +0800

    user

---
+++ gmall-user/src/main/java/com/edward/gmall/user/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.edward.gmall.user.controller;
+
+/**
+ * @author ChenGuiHong
+ * @create 2020-08-20  16:48
+ */
+public class UserController {
+}
---
+++ gmall-user/src/main/java/com/edward/gmall/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.edward.gmall.user.service.impl;
+
+import com.edward.gmall.user.service.UserService;
+
+/**
+ * @author ChenGuiHong
+ * @create 2020-08-20  16:49
+ */
+public class UserServiceImpl implements UserService {
+
+
+}
