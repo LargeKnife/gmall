966fcb1aee33a013a6f953ad23a435e3f7afd73f
EdwardGit <pocketknifegithub@163.com>
2020-09-04 12:43:50 +0800

    user

--- gmall-user/src/main/java/com/edward/gmall/user/controller/UserController.java
+++ gmall-user/src/main/java/com/edward/gmall/user/controller/UserController.java
@@ -1,8 +1,35 @@
 package com.edward.gmall.user.controller;
 
+import com.edward.gmall.user.bean.UmsMember;
+import com.edward.gmall.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
 /**
  * @author ChenGuiHong
- * @create 2020-08-20  16:48
+ * @create 2020-09-04  11:45
  */
+@Controller
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("/index")
+    @ResponseBody
+
+    public String hello(){
+        return "hello";
+    }
+
+    @RequestMapping("/getAll")
+    @ResponseBody
+    public List<UmsMember> getAllUser(){
+        List<UmsMember>  list = userService.getAllUser();
+        return list;
+    }
 }
---
+++ gmall-user/src/main/java/com/edward/gmall/user/service/UserService.java
@@ -0,0 +1,13 @@
+package com.edward.gmall.user.service;
+
+import com.edward.gmall.user.bean.UmsMember;
+
+import java.util.List;
+
+/**
+ * @author ChenGuiHong
+ * @create 2020-09-04  11:54
+ */
+public interface UserService {
+    List<UmsMember> getAllUser();
+}
